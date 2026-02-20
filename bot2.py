42b3e3b3770b5ebeb55cad3b7c1ce6b9b399e8b3
Niunek <niuneka7@gmail.com>
2026-02-20 22:12:07 +0100

    d

--- bot2.py
+++ bot2.py
@@ -55,17 +55,17 @@ async def on_message(message):
 async def lvl(ctx):
     user_id = str(ctx.author.id)
     xp_usera =lista[user_id]
-    name_usera = ctx.author.nick
+    name_usera = ctx.author.global_name
     avatar_img = load_image(ctx.author.display_avatar.url)
-    avatar = Editor(avatar_img).resize((150,150)).circle_image()
+    avatar = Editor(avatar_img).resize((300,300)).circle_image()
     tlo = Editor("buckground.png")
     tlo.paste(avatar, (200, 500))
 
     profil_czcionki_1 = Font.poppins(size = 50, variant = "regular")
     profil_czcionki_2 = Font.poppins(size = 40, variant = "light")
     tekst_poziomu = f"lvl: {xp_usera}"
-    tlo.text((210, 660), str(name_usera),font = profil_czcionki_1, color="white")
-    tlo.text((210, 720), str(tekst_poziomu),font = profil_czcionki_2, color="red")
+    tlo.text((210, 860), str(name_usera),font = profil_czcionki_1, color="white")
+    tlo.text((210, 920), str(tekst_poziomu),font = profil_czcionki_2, color="red")
 
     plik = discord.File(fp=tlo.image_bytes,filename="poziom.png")
     await ctx.send(file=plik)
